5e412e2bbc1c810939171cd2a15d31fbd1e4432c
Artyom Kim <71181763+moymik@users.noreply.github.com>
2024-01-30 01:27:21 +0900

    Navbar Button restyled

--- src/widgets/navbar/navbar.tsx
+++ src/widgets/navbar/navbar.tsx
@@ -1,21 +1,25 @@
 import { AppBar, Toolbar, Button } from "@mui/material";
+
 import { useNavigate } from "react-router";
 
 export default function NavBar() {
   const navigate = useNavigate();
   return (
-    <AppBar 
-        position = "fixed"
-        sx = {{height:"8vh"}}
-    >
+    <AppBar position="fixed" sx={{ height: "8vh" }}>
       <Toolbar>
         <Button
           color="inherit"
+          size="large"
+          sx={{
+            color: "black",
+            fontWeight: 600,
+            backgroundColor: "white",
+          }}
           onClick={() => {
             navigate("/posts");
           }}
         >
-          Home
+          Posts
         </Button>
       </Toolbar>
     </AppBar>
